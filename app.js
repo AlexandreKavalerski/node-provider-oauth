06d7b07b5338d4d331119321cf9df1eb98f439a9
Alexandre <kavalerskialexandre@gmail.com>
2017-12-16 00:58:02 -0300

    Adicionando rota GET para par de chaves

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var index = require('./routes/index');
 var users = require('./routes/usuarios');
 var auth = require('./routes/auth');
 var appClientes = require('./routes/appClientes');
+var chavesUsuarios = require('./routes/chavesUsuarios');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/app', appClientes);
 app.use('/auth', auth);
+app.use('/keys', chavesUsuarios);
 
 
 // catch 404 and forward to error handler
