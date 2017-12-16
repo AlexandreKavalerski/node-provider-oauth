492cba4c1db22141523961e75b29c5b13da23894
Alexandre <kavalerskialexandre@gmail.com>
2017-12-16 01:00:21 -0300

    Limpando código

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -13,29 +13,16 @@ router.get('/public', function(req, res, next) {
   });
 });
 
-router.get('/:login/keys', function(req, res, next) {
-  var login = req.params.login;
-  Usuario.findOne({'login':login}, function(erro, usuario) {
-    if (erro)
-      res.send(erro);
-    res.json({"login": usuario.login, "chavePublica": utils.decriptFromHex(usuario.chavePublica), "chavePrivada":  utils.decriptFromHex(usuario.chavePrivada)});
-  });
-});
-
-
 router.post('/', function(req, res, next) {
   var usuario = new Usuario();
   
   usuario.login = req.body.login;
   usuario.senha = require('crypto').createHash('md5').update(req.body.senha).digest('hex');
   usuario.chavePrivada = utils.criptoToHex(req.body.chavePrivada);
-  // usuario.chavePrivada = require('crypto').createHash('md5').update(req.body.chavePrivada).digest('hex');
   usuario.chavePublica = utils.criptoToHex(req.body.chavePublica);
-  // usuario.chavePublica = require('crypto').createHash('md5').update(req.body.chavePublica).digest('hex');
 
   usuario.save(function(erro) {
       if (erro){
-          console.log(erro);
           res.send(erro);
       }
       else{
