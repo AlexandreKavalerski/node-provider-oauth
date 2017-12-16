3ccff67550ca465318b8013023f85463721ed52f
Alexandre <kavalerskialexandre@gmail.com>
2017-12-16 01:00:07 -0300

    Adicionando campo 'nivelAcesso' ao model de aplicacação cliente

--- models/appCliente.js
+++ models/appCliente.js
@@ -8,7 +8,8 @@ mongoose.connect(mongodb, {
 var AppClienteSchema = new mongoose.Schema({
   nome: { type: String, unique: true, required: true },
   urlCallback: { type: String, required: true},
-  chave: {type: String, required: true, unique: true}
+  chave: {type: String, required: true, unique: true},
+  nivelAcesso: {type: String, required: false}
 });
 
 module.exports = mongoose.model('AppCliente', AppClienteSchema);
